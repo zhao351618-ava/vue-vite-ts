6a39436b065c9a3ecb349339cf3ab49fd8529063
zhao351618-ava <17753437458@163.com>
2025-03-11 20:38:48 +0800

    Add files via upload

---
+++ src/api/user/index.ts
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+import type {
+  LoginFormData,
+  LoginResponseData,
+  userInfoResponseData,
+} from './type'
+
+
+
+enum API {
+  LOGIN_URL = '/admin/acl/index/login',
+  USERINFO_URL = '/admin/acl/index/info',
+  LOGOUT_URL = '/admin/acl/index/logout',
+}
+
+// 登录接口
+export const reqLogin = (data: LoginFormData) =>
+  request.post<any, LoginResponseData>(API.LOGIN_URL, data)
+
+export const reqUserInfo = () =>
+  request.get<any, userInfoResponseData>(API.USERINFO_URL)
+
+export const reqLogOut = () => request.post<any, any>(API.LOGOUT_URL)
